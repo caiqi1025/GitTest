7a96140841a50d8be4b8e62ee51ba28d2f47cf35
caiqi <596801982@qq.com>
2018-08-09 15:38:27 +0800

    Idea使用Token测试

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Welcome to GitHub!");
+        System.out.println("Welcome to GitHub!");
     }
 }
